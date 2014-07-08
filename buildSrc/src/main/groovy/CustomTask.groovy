090f24bc3fc20db7d8ec5b119705b120b5a266bd
Kieran Murphy <kieran.murphy@ie.ibm.com>
2014-07-08 11:30:53 +0100

    Updated custom tasks code

--- buildSrc/src/main/groovy/CustomTask.groovy
+++ buildSrc/src/main/groovy/CustomTask.groovy
@@ -2,24 +2,12 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class CustomTask extends DefaultTask {
-  def hostname = 'localhost'
-  def port = 3306
-  def sql
-  def database
-  def username = 'root'
-  def password = 'password'
+  def message = "Hello from CustomTask"
 
   @TaskAction
   def runQuery() {
     def cmd
-        if(database) {
-          cmd = "mysql -u ${username} -p${password} -h ${hostname} -P ${port} ${database} -e "
-        }
-        else {
-          cmd = "mysql -u ${username} -p${password} -h ${hostname} -P ${port} -e "
-        }
-        project.exec {
-          commandLine = cmd.split().toList() + sql
-        }
+        // do whatever you want here...
+        println "${message}"
   }
 }
